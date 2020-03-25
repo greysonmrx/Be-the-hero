f8c7b6ef8c586c671e8c4695d6aef66d3719a1a8
unknown <greysonmrx@gmail.com>
2020-03-25 15:05:06 -0300

    feat: 🎸 create route to add a new ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,24 @@
 const { Router } = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 
 const routes = Router();
 
-routes.get("/", (req, res) => res.json({ message: "Hello World!" }));
+routes.post("/ongs", async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString("hex");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return res.json({ id });
+});
 
 module.exports = routes;
