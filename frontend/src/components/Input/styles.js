33441feda89d84fe5136f4ffbb05c427efb1ca7c
unknown <greysonmrx@gmail.com>
2020-03-25 17:07:09 -0300

    feat: 🎸 (frontend) style input component

--- frontend/src/components/Input/styles.js
+++ frontend/src/components/Input/styles.js
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const StyledInput = styled.input``;
+export const StyledInput = styled.input`
+  width: 100%;
+  height: 60px;
+  color: #333333;
+  border: 2px solid #dcdce6;
+  border-radius: 8px;
+  padding: 0 24px;
+
+  &:focus {
+    border-color: #e02041;
+    transition: 0.2s ease-out;
+  }
+`;
