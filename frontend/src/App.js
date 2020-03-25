c8873222e0f3b3232acff3f4ef3f7220796e0303
unknown <greysonmrx@gmail.com>
2020-03-25 18:47:09 -0300

    feat: 🎸 (frontend) configure routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import GlobalStyle from "./styles/global";
 
+import Routes from "./routes";
+
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Routes />
     </>
   );
 }
---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch></Switch>
+    </BrowserRouter>
+  );
+}
