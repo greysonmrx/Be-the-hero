55df4d0fa37d047dfd66b85f1a55eed150975b93
unknown <greysonmrx@gmail.com>
2020-03-25 17:18:39 -0300

    feat: 🎸 (frontend) style button component

--- frontend/src/components/Button/styles.js
+++ frontend/src/components/Button/styles.js
@@ -1,3 +1,22 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
-export const StyledButton = styled.button``;
+export const StyledButton = styled.button`
+  width: 100%;
+  height: 60px;
+  background: #e02041;
+  border: 0;
+  border-radius: 8px;
+  color: #ffffff;
+  font-weight: 700;
+  margin-top: 16px;
+  transition: background 0.2s;
+  display: inline-block;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 60px;
+
+  &:hover {
+    background: ${darken(0.1, "#e02041")};
+  }
+`;
