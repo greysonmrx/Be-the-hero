31e1080b3c1d125197c9fee4637cb9e0105fc0cf
unknown <greysonmrx@gmail.com>
2020-03-26 19:58:49 -0300

    feat(mobile): style detail page

--- mobile/src/pages/Detail/styles.js
+++ mobile/src/pages/Detail/styles.js
@@ -1,27 +1,80 @@
 import styled from "styled-components/native";
+import Constants from "expo-constants";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  padding-top: ${Constants.statusBarHeight + 20}px;
+`;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const Img = styled.Image``;
 
 export const BackButton = styled.TouchableOpacity``;
 
-export const Incident = styled.View``;
+export const Incident = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  margin-bottom: 16px;
+  margin-top: 48px;
+`;
 
-export const Property = styled.Text``;
+export const Property = styled.Text`
+  font-size: 14px;
+  color: #41414d;
+  font-weight: bold;
+  margin-top: 24px;
+`;
 
-export const Value = styled.Text``;
+export const Value = styled.Text`
+  margin-top: 8px;
+  font-size: 15px;
+  color: #737380;
+`;
 
-export const ContactBox = styled.View``;
+export const ContactBox = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  margin-bottom: 16px;
+`;
 
-export const HeroTitle = styled.Text``;
+export const HeroTitle = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+  color: #13131a;
+  line-height: 30px;
+`;
 
-export const HeroDescription = styled.Text``;
+export const HeroDescription = styled.Text`
+  font-size: 15px;
+  color: #737380;
+  margin-top: 16px;
+`;
 
-export const Actions = styled.View``;
+export const Actions = styled.View`
+  margin-top: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-export const Action = styled.TouchableOpacity``;
+export const Action = styled.TouchableOpacity`
+  background-color: #e02041;
+  border-radius: 8px;
+  height: 50px;
+  width: 48%;
+  justify-content: center;
+  align-items: center;
+`;
 
-export const ActionText = styled.Text``;
+export const ActionText = styled.Text`
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: bold;
+`;
